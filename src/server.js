7dfa48ce0b7e2ebbafeea249d4dce6984d3ea056
John Heiden <jdh5563@rit.edu>
2022-01-29 00:23:54 -0500

    Did step 7 of the homework.

---
+++ src/server.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const mediaHandler = require('./mediaResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
